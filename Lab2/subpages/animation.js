492cce363280f57b386f96856d3b1ef7339781ca
Adam Krizar <adam.krizar@gmail.com>
2020-04-03 22:09:07 +0200

    animacja

--- Lab2/subpages/animation.js
+++ Lab2/subpages/animation.js
@@ -1,14 +1,44 @@
-function animate(statek, i)
+function animate_left(statek, i)
 {
-    left = statek.style.left;
-    left = left.replace("px", "");
-    value = parseInt(left);
-    statek.style.left = value + 10 + "px";
-    console.log(i);
+    setTimeout(function()
+    {
+        i++;
+        left = statek.style.left;
+        left = left.replace("px", "");
+        value = parseInt(left);
+        statek.style.left = value + 10 + "px";
+        if(i == 41)
+        {
+            statek.src="resources/rocketship2.png";
+            animate_right(statek, 0); 
+        }
+        else animate_left(statek, i);
+    }, 100)
+}
+
+function animate_right(statek, i)
+{
+    setTimeout(function()
+    {
+        i++;
+        left = statek.style.left;
+        left = left.replace("px", "");
+        value = parseInt(left);
+        statek.style.left = value - 10 + "px";
+        if(i == 41) 
+        {
+            statek.src="resources/rocketship.gif";
+            statek.style.display = "none";
+            document.getElementById("start").style.visibility = "visible";
+            return;
+        }
+        else animate_right(statek, i);
+    }, 100)
 }
 
 function startAnimation()
 {
+    document.getElementById("start").style.visibility = "hidden";
     statek = document.getElementById("ship");
     stars = document.getElementById("background");
     statek.style.display = "block";
@@ -16,12 +46,6 @@ function startAnimation()
     
     statek.style.left = rect.left + 1 + "px";
     statek.style.top = rect.top + 17 + "px";
-    statek.style.position = "absolute"
-    for(let i = 0; i < 10; i++)
-    {
-        var test = setTimeout(animate(statek, i),1000);
-    }
-    //statek.style.left = "400" + "px";
-    //statek.style.left = bounds.left;
-    
+    statek.style.position = "absolute";
+    animate_left(statek, 0);   
 }
