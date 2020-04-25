e643feb92a1914361bbef567b88868f4bfc2c849
Adam Krizar <adam.krizar@gmail.com>
2020-04-25 12:31:49 +0200

    ball + gameover

--- Lab3/subpages/breakout.js
+++ Lab3/subpages/breakout.js
@@ -7,16 +7,71 @@ var ball;
 var blocks;
 
 var cx = 0;
+var px = 0;
 
-var released = false;
+var bx = 0;
+var by = 670;
 
-function onRelease() {}
+var palletx;
+
+var zx = 1;
+var zy = -1;
+
+var launch = false;
+
+function onRelease() 
+{
+    if(!launch)
+    {
+        if(cx < px) zx = -1;
+        else zx = 1;
+        launch = true;
+        moveBall();
+    }
+}
 
 function onMove() 
 {
+    px = cx;
     cx = event.pageX - pallet.offsetLeft;
     drawPallet();
-    if(!released) drawBall;
+    if(!launch) drawBall();
+}
+
+function moveBall()
+{
+    setTimeout(function()
+    {
+        if(bx >= 1395) zx = -zx;
+        if(bx <= 5) zx = -zx;
+
+        if(by <= 5) zy = -zy;
+
+        if(by == 675)
+        {
+            if(bx > palletx && bx < (palletx + 150)) zy = -zy;
+        }
+
+        if(by > 700)  endGame();
+
+        bx = bx + zx;
+        by = by + zy;
+        drawBall(bx, by);
+        if(launch)
+        {
+            moveBall();
+        }
+    }, 1)
+}
+
+function endGame()
+{
+    launch = false;
+    document.getElementById("spacer").style.display = "none";
+    document.getElementById("isgame").style.display = "none";
+    document.getElementById("nogame").style.display = "block";
+    document.getElementById("lost").style.display = "block";
+    by = 670;
 }
 
 function drawPallet()
@@ -24,6 +79,8 @@ function drawPallet()
     xx = cx - 75;
     if(xx < 0) xx = 0;
     if(xx > 1250) xx = 1250;
+
+    palletx = xx;
 
     ctx.clearRect(0, 0, pallet.width, pallet.height);
     ctx.beginPath();
@@ -34,14 +91,18 @@ function drawPallet()
 
 function drawBall()
 {
-    xx = cx - 75;
-    if(xx < 0) xx = 0;
-    if(xx > 1250) xx = 1250;
+    if(!launch)
+    {
+        bx = cx;
+        if(bx < 75) bx = 75;
+        if(bx > 1325) bx = 1325;
+    }    
 
     ctx2.clearRect(0, 0, ball.width, ball.height);
     ctx2.beginPath();
-    ctx2.arc(xx, 100, 50, 0, 2 * Math.PI, false);
+    ctx2.arc(bx, by, 10, 0, 2 * Math.PI, false);
     ctx2.fillStyle = "white";
+    ctx2.strokestyle = "white"
     ctx2.fill();
     ctx2.stroke();
 }
@@ -51,8 +112,9 @@ function startGame()
     document.getElementById("spacer").style.display = "block";
     document.getElementById("isgame").style.display = "block";
     document.getElementById("nogame").style.display = "none";
+    document.getElementById("lost").style.display = "none";
 
-    document.getElementById("spacer").style.height = 1000 + "px";
+    document.getElementById("spacer").style.height = 720 + "px";
 
     pallet = document.getElementById("pallet");
     ball = document.getElementById("ball");
