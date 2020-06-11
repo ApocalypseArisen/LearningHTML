710a9c590ec3169b3c16dc7619181e6415867ff2
Adam Krizar <adam.krizar@gmail.com>
2020-06-11 19:45:51 +0200

    wow actually doing something

--- lab6/zad3/src/index.js
+++ lab6/zad3/src/index.js
@@ -1,9 +1,102 @@
-var http = require('http');
+const http = require('http');
+const fs = require("fs");
+const os = require("os");
+const joke = require('one-liner-joke');
 
-http.createServer(function (req, res) 
+const port = 8080
+
+function dateTime()
 {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-}).listen(8080);
+  let date = new Date();
+  let day = ("0" + date.getDate()).slice(-2);
+  let month = ("0" + (date.getMonth() + 1)).slice(-2);
+  let year = date.getFullYear();
+  let dt = "Obecna data to: " + day + "/" + month + "/" + year;
+  return dt;
+}
 
-console.log("Serwer uruchomiony na: localhost:8080")
+function cpuInfo()
+{
+  obj = JSON.stringify(os.cpus());
+  return obj;
+}
+
+function serverInfo()
+{
+  var envData = {
+    FreeMemory: os.freemem(),
+    HomeDirectory: os.homedir(),
+    Hostname: os.hostname(),
+    Platform: os.platform(),
+    TotalMeory: os.totalmem(),
+    System: os.type(),
+    Uptime: os.uptime()
+  }
+
+  obj = JSON.stringify(envData)
+  return obj;
+}
+
+function tellJoke()
+{
+  funny = joke.getRandomJoke({'exclude_tags': ['dirty', 'racist', 'marriage']});
+  return funny.body;
+}
+
+function getData()
+{
+  try
+  {
+    let data = fs.readFileSync('./assets/data.json');
+    let plan = JSON.parse(data);
+    obj = JSON.stringify(plan);
+    return obj;
+  }
+  catch (error)
+  {
+    console.log(error);
+    return "Error";
+  }
+}
+
+function errorPage(response)
+{
+  try
+  {
+    let data = fs.readFileSync('./src/html/error.html');
+    response.writeHeader(200, {"Content-Type": "text/html"});  
+    response.write(data);  
+    response.end();  
+  }
+  catch (error)
+  {
+    console.log(error);
+    response.end("404 - Nie znaleziono podanej strony");
+  }
+}
+
+const requestHandler = (request, response) => {
+  console.log(request.url)
+  switch(request.url)
+  {
+    case "/": response.end("MAIN PAGE"); break;
+    case "/ping": response.end("pong"); break;
+    case "/datetime": response.end(dateTime()); break;
+    case "/cpus": response.end(cpuInfo()); break;
+    case "/env": response.end(serverInfo()); break;
+    case "/joke": response.end(tellJoke()); break;
+    case "/somedata": response.end(getData()); break;
+    default: errorPage(response);
+  }
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => {
+  if(err)
+  {
+    return console.log("ERROR: Awaria serwera!");
+  }
+
+  console.log("Serwer działa na localhost:", port)
+})
