b3a96a53bd5bf55e35db5de2a69eb7231ca9eb37
Adam Krizar <adam.krizar@gmail.com>
2020-06-13 11:55:21 +0200

    zadania do właściwych folderów

--- lab6/zad1/src/index.js
+++
@@ -1,9 +0,0 @@
-var math = require('./math');
-
-var args = process.argv.slice(2);
-arg = args[0];
-
-if(!isNaN(arg) && arg !== null) math.fibonacci(arg);
-else console.log("Błędne dane");
-
-process.exitCode = 1;
--- lab6/zad2/src/index.js
+++ lab6/zad2/src/index.js
@@ -1,3 +1,9 @@
-var axe = require('./axiosTest');
+var math = require('./math');
 
-axe.axTest();
+var args = process.argv.slice(2);
+arg = args[0];
+
+if(!isNaN(arg) && arg !== null) math.fibonacci(arg);
+else console.log("Błędne dane");
+
+process.exitCode = 1;
